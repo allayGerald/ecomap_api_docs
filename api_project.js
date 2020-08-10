d9814d9f6e5f17ffbba7bf56cd243d9b74f45a02
Gerald Allay <geraldallay@gmail.com>
2020-08-10 11:14:42 +0300

    Feat signup documentation

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-08-07T23:50:45.362Z",
+    "time": "2020-08-10T08:12:43.607Z",
     "url": "http://apidocjs.com",
     "version": "0.24.0"
   }
