8b078232bcbee9e0778991074d3072769e8dfce8
Gerald Allay <geraldallay@gmail.com>
2020-09-07 11:38:30 +0300

    DOCS: Add filter docs

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-09-06T07:24:39.217Z",
+    "time": "2020-09-07T08:37:53.512Z",
     "url": "http://apidocjs.com",
     "version": "0.24.0"
   }
