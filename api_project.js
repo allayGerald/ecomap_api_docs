2df0741cc0130b6d1bce42c003fb6f1c7698dce8
Gerald Allay <geraldallay@gmail.com>
2020-11-09 18:21:58 +0300

    Add audit trail docs

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-11-04T16:26:22.318Z",
+    "time": "2020-11-09T15:18:40.569Z",
     "url": "http://apidocjs.com",
     "version": "0.24.0"
   }
